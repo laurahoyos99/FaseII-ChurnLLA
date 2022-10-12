9c6ebf26a8ed051d7423ddbce4e4723d0adbcc9c
laurahoyos99 <97916353+laurahoyos99@users.noreply.github.com>
2022-10-12 16:01:30 -0500

    Create Cap_Fase3_Ejercicio1.sql

---
+++ Capacitacion/Fase III/Cap_Fase3_Ejercicio1.sql
@@ -0,0 +1,28 @@
+with
+ejercicio1 as(
+SELECT distinct date_trunc('month',date(dt)) as month,act_acct_cd
+FROM "db-analytics-prod"."fixed_cwp" 
+where act_cust_typ_nm = 'Residencial' -- filtro para clientes residenciales
+and date(dt)=date_trunc('month',date(dt)) --filtro para tomar el 1er día de cada mes
+and year(date(dt))=2022 -- filtro para extraer únicamente los datos del 2022
+)
+,interactions as(
+select distinct date_trunc('month',date(INTERACTION_START_TIME)) as month,account_id,interaction_id
+from "db-stage-prod"."interactions_cwp"
+)
+,interactions_per_user as(
+select distinct a.month,act_acct_cd,count(distinct interaction_id) as interactions 
+-- se agrupan las llamadas por mes y contrato para saber cuantás llamadas tiene cada cliente por mes
+from ejercicio1 a inner join interactions b on a.act_acct_cd=b.account_id and a.month=b.month
+group by 1,2 order by 1,2
+)
+,categories as(
+select distinct *
+,case when interactions=1 then 'Usuarios = 1 llamada'
+      when interactions>1 then 'Usuarios > 1 llamada'
+else null end as Interactions_Tier --Creación de las categorías
+from interactions_per_user
+)
+select distinct month,interactions_tier,count(distinct act_acct_cd) as users
+from categories
+group by 1,2 order by 1,2
