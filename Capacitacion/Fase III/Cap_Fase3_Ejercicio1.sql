88f93779e1603cd68f4cbb2c2f1e501aa9a6e399
laurahoyos99 <97916353+laurahoyos99@users.noreply.github.com>
2022-10-12 16:55:20 -0500

    Update Cap_Fase3_Ejercicio1.sql

--- Capacitacion/Fase III/Cap_Fase3_Ejercicio1.sql
+++ Capacitacion/Fase III/Cap_Fase3_Ejercicio1.sql
@@ -3,7 +3,6 @@ ejercicio1 as(
 SELECT distinct date_trunc('month',date(dt)) as month,act_acct_cd
 FROM "db-analytics-prod"."fixed_cwp" 
 where act_cust_typ_nm = 'Residencial' -- filtro para clientes residenciales
-and date(dt)=date_trunc('month',date(dt)) --filtro para tomar el 1er día de cada mes
 and year(date(dt))=2022 -- filtro para extraer únicamente los datos del 2022
 )
 ,interactions as(
